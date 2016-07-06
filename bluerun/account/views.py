62c8c9f6bdad3f2853a5773a9f6505c2a9533b5d
neelansh <neelansh5@gmail.com>
2016-07-06 21:20:53 +0530

    forgot password flow added

--- bluerun/account/views.py
+++ bluerun/account/views.py
@@ -4,7 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.core.urlresolvers import reverse
 from account.forms import *
-
+from .models import *
+from django.template import loader
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+from django.http import Http404, JsonResponse, HttpResponse
 # Create your views here.
 
 def signup(request):
@@ -13,21 +17,59 @@ def signup(request):
 @require_http_methods(['GET', 'POST'])
 def login(request):
 	if request.user.is_authenticated():
-		return redirect(reverse('dashboard', kwargs={'id': request.user.id}));
+		return redirect(reverse('dashboard', kwargs={'id': request.user.id}))
 	if request.method == 'GET':
-		context = { 'f' : LoginForm()};
-		return render(request, 'account/login.html', context);
+		context = { 'f' : LoginForm()}
+		return render(request, 'account/login.html', context)
 	else:
-		f = LoginForm(request.POST);
+		f = LoginForm(request.POST)
 		if not f.is_valid():
-			return render(request, 'account/login.html', {'f' : f});
+			return render(request, 'account/login.html', {'f' : f})
 		else:
 			user = f.authenticated_user
 			auth_login(request, user)
-			return redirect(reverse('dashboard', kwargs = {'id': user.id}));
+			return redirect(reverse('dashboard', kwargs = {'id': user.id}))
+
 
 def forgot_password(request):
-	pass
+	if request.user.is_authenticated():
+		return redirect(reverse('dashboard', kwargs={'id': request.user.id}))
+	if request.method == 'GET':
+		context = { 'f' : ForgotPassword()}
+		return render(request, 'account/forgot_password.html', context)
+	else:
+		f = ForgotPassword(request.POST)
+		if not f.is_valid():
+			return render(request, 'account/forgot_password.html', {'f' : f})
+		else:
+			user = MyUser.objects.get(username = f.cleaned_data['username'])
+			otp = create_otp(user = user, purpose = 'FP')
+			email_body_context = { 'u' : user, 'otp' : otp}
+			body = loader.render_to_string('account/forgot_password_email.txt', email_body_context)
+			print(user.email)
+			message = EmailMultiAlternatives("Reset Password", body, 'bluerunfinancial@gmail.com', [user.email])
+			#message.attach_alternative(html_body, 'text/html')
+			message.send()
+			return render(request, 'account/forgot_email_sent.html', {'u': user})
+
+def set_password(request, id = None, otp = None):
+    if request.user.is_authenticated():
+    	return redirect(reverse('dashboard', kwargs={'id': request.user.id}))
+    user = get_object_or_404(MyUser, id=id)
+    otp_object = get_valid_otp_object(user = user, purpose='FP', otp = otp)
+    if not otp_object:
+        raise Http404()
+    if request.method == 'GET':
+        f = SetPasswordForm()
+    else:
+        f = SetPasswordForm(request.POST)
+        if f.is_valid():
+            user.set_password(f.cleaned_data['new_password'])
+            user.save()
+            otp_object.delete()
+            return render(request, 'account/set_password_success.html', { 'u' : user})
+    context = { 'f' : f, 'otp': otp_object.otp, 'uid': user.id}
+    return render(request, 'account/set_password.html', context)
 
 @login_required(login_url = 'login')
 @require_http_methods(['GET' , 'POST'])
@@ -47,4 +89,4 @@ def reset_password(request , id):
 @login_required(login_url =  'login')
 def logout(request , id):
     auth_logout(request)
-    return redirect(reverse('login'));
+    return redirect(reverse('login'))
