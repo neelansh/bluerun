9b7eb645dceb5cfdfdcc1c9b98e8a80b9b87cfbf
neelansh <neelansh5@gmail.com>
2016-09-08 22:41:16 +0530

    removed groups from admin

--- bluerun/account/admin.py
+++ bluerun/account/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import MyUser, UserOTP
+from django.contrib.auth.models import Group
 
 # Register your models here.
 @admin.register(MyUser)
 class MyUserAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'email']
+
+admin.site.unregister(Group)
