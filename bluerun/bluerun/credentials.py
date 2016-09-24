caef2cf5ffd454b091f2d448410d38a3a48cc721
neelansh <neelansh5@gmail.com>
2016-09-24 18:36:35 +0530

    deploying

---
+++ bluerun/bluerun/credentials.py
@@ -0,0 +1,13 @@
+database_name = 'bluerun'
+database_user = 'root'
+database_password = 'neelansh5'
+
+secret_key = '$76b+_t*j_%$15(_96v(r=1u4yelple=ds^!w-raio8^2qys7w'
+
+email_host = 'email-smtp.eu-west-1.amazonaws.com'
+email_host_user = 'AKIAIE7NL252XBZVAUOA'
+email_host_password = 'AoFvYRAvkC4e6oSVJBq0Dlu9hur0zAX4k4X0UWtCEfeS'
+
+debug_state = True
+
+allowed_host = []
